98248a7bc38553289241d625f53f8a8edce804c1
haijun.wang <haijun.wang@dhc.com.cn>
2023-02-06 21:31:29 +0800

    23/02/06

---
+++ zack/Note/Lambda表达式/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// 委托指向匿名方法
+Action a1 = delegate ()
+{
+    Console.WriteLine("Hello, World!");
+};
+Func<int, string, string> f1 = delegate (int age, string name)
+{
+    return (name + " :" + age + "岁");
+};
+
+//匿名方法写成lambda表达式,省略delegate
+Func<int, int, int> f2 = (int i, int j) =>
+{
+    return (i + 1) + j;
+};
+//可以根据委托类型推断lambda表达式类型，可以省略委托类型
+Func<int, int, int> f3 = (i,j) =>
+{
+    return (i + 1) + j;
+};
+a1();
+Console.WriteLine(f1(18, "niko"));
+Console.WriteLine(f2(18, 17));
+Console.WriteLine(f3(10, 7));
---
+++ zack/Note/WhenAll/Program.cs
@@ -0,0 +1,48 @@
+﻿// See https://aka.ms/new-console-template for more information
+string[] files = Directory.GetFiles(@"d:\learn\zack\Note\temp");
+List<Task<int>> counts = new List<Task<int>>();
+
+foreach(var file in files)
+{
+    var result = ReadCharCount(file);
+    counts.Add(result);
+}
+// when all 多个异步方法都执行结束后这个Task结束
+var allTask = await Task.WhenAll(counts);
+var sum = allTask.Sum();
+Console.WriteLine(sum.ToString());
+
+Console.WriteLine("______________________");
+foreach(var s in Test())
+{
+    Console.WriteLine(s);
+}
+Console.WriteLine("______________________");
+//yield 在异步方法中，添加await 修饰调用
+await foreach (var s in Test2())
+{
+    Console.WriteLine(s);
+}
+
+
+static async Task<int> ReadCharCount(string fileName)
+{
+    var s = await File.ReadAllTextAsync(fileName);
+    return s.Length;
+}
+//yield 也是拆分成状态机的方式执行
+static IEnumerable<string> Test()
+{
+    yield return "1";
+    yield return "2";
+    yield return "3";
+
+}
+
+static async IAsyncEnumerable<string> Test2()
+{
+    yield return "1";
+    yield return "2";
+    yield return "3";
+
+}
