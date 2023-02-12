78d482f61eea3c798fca3bae2fd76fbd95467e87
haijun.wang <haijun.wang@dhc.com.cn>
2023-02-12 20:39:09 +0800

    Log

---
+++ zack/Note/LoggingDemo/LogTest.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoggingDemo
+{
+    public class LogTest
+    {
+        private readonly ILogger<LogTest> _logger;
+        public LogTest(ILogger<LogTest> logger) { 
+         _logger= logger;
+        }
+        public void Test()
+        {
+            _logger.LogDebug("开始执行数据库同步");
+            _logger.LogError("同步数数据库错误！");
+        }
+    }
+}
---
+++ zack/Note/LoggingDemo/Program.cs
@@ -0,0 +1,20 @@
+﻿
+using LoggingDemo;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using NLog.Extensions.Logging;
+
+ServiceCollection service= new ServiceCollection();
+//把日志加到控制台
+service.AddLogging(logBuilder => {
+   logBuilder.AddConsole();
+   logBuilder.SetMinimumLevel(LogLevel.Debug);
+   //使用nlog记录到文件
+   logBuilder.AddNLog();
+});
+service.AddScoped(typeof(LogTest));
+using (var sp = service.BuildServiceProvider())
+{
+    var logTest = sp.GetService<LogTest>();
+    logTest?.Test();
+}
