7227f9b01fa28d8f035929c3a63989305a4d0dc8
haijun.wang <haijun.wang@dhc.com.cn>
2023-02-12 17:18:31 +0800

    配置文件

---
+++ zack/Note/配置文件/Program.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using 配置文件;
+
+ServiceCollection service = new ServiceCollection();
+service.AddScoped(typeof(TestController));
+//使用 ConfigurationBuilder 类进行配置文件的读取
+ConfigurationBuilder configuration = new ConfigurationBuilder();
+//添加配置文件，设置属性
+//json文件属性 设置为如果较新则复制，因为debugger状态下读取的是debug这个文件夹下的文件，如此设置可以复制json文件到debug文件夹下
+configuration.AddJsonFile("config.json", true, true);
+IConfigurationRoot root = configuration.Build();
+var name = root["name"];
+var address =  root.GetSection("proxy:address").Value;
+Console.WriteLine(name);
+Console.WriteLine(address);
+
+//通过DI注册AddOptions方法，并把root绑定到config
+service.AddOptions().Configure<Config>(e => root.Bind(e));
+
+using (var sp = service.BuildServiceProvider())
+{
+    var testController = sp.GetService<TestController>();
+    Console.WriteLine("________________________________");
+    testController?.Test();
+}
+
+
+
+public class Config
+{
+    public string? Name { get; set; }
+    public string? Age { get; set; }
+    public Proxy? Proxy { get; set; }
+}
+public class Proxy
+{
+    public string? Address { get; set; }
+    public string? Port { get; set; }
+}
---
+++ zack/Note/配置文件/TestController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 配置文件
+{
+    public class TestController
+    {
+        private readonly IOptionsSnapshot<Config> _optConfig;
+
+        public TestController(IOptionsSnapshot<Config> optConfig)
+        {
+            _optConfig = optConfig ?? throw new ArgumentNullException();
+        }
+        public void Test()
+        {
+            Console.WriteLine("name:{0}",_optConfig.Value.Name);
+            Console.WriteLine("age:{0}",_optConfig.Value.Age);
+            Console.WriteLine("ipAddress:{0}",string.Concat(_optConfig?.Value?.Proxy?.Address,":",_optConfig?.Value?.Proxy?.Port));
+        }
+    }
+}
